d4044188a49c4cc29643c9307685ea2c46933135
abirukov2008 <abirukov2008@gmail.com>
2023-02-13 22:01:10 +0300

    правка по env

--- config.py
+++ config.py
@@ -3,12 +3,17 @@ import random
 from typing import Mapping, Any
 
 from game_enums import Color
+import os
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 
 def get_config() -> Mapping[str, Any]:
     config = {
-        "GAME_FIELD_SIZE": (os.environ.get("GAME_FIELD_SIZE", 3)),
-        "COUNT_PLAYERS": (os.environ.get("COUNT_PLAYERS", 2)),
+        "GAME_FIELD_SIZE": os.environ.get("GAME_FIELD_SIZE", 3),
+        "COUNT_PLAYERS": os.environ.get("COUNT_PLAYERS", 2),
         "BOT_NAME": os.environ.get("BOT_NAME", "Бот"),
         "PLAYER_SYMBOLS": os.environ.get("PLAYER_SYMBOLS", "X,O").split(",")
     }
